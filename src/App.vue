47c12b79ef9cde7bd1fcd4fcb0a3073d3fcaaca0
zeynab <zeynab.sharifi.1378@gmail.com>
2022-01-15 15:03:42 -0800

    create page components and header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 
 <template>
+  <HeaderTodo></HeaderTodo>
   <router-view></router-view>
 </template>
 <script>
+
+import HeaderTodo from "./components/Header-todo.vue";
+
   export default{
     name:'App',
+    components:{
+    HeaderTodo,
+}
   }
 </script>
 <style>
