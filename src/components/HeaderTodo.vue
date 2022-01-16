03ac175d1e551eba239f1c80c896ebf6167ec30d
zeynab <zeynab.sharifi.1378@gmail.com>
2022-01-15 16:17:02 -0800

    install sass and create mixins, varaible, headertodo components

---
+++ src/components/HeaderTodo.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+      <div class="header-todo">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>
+                        my Todo List
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from'./router.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../public/style/css/main.css'
 
 const app = createApp(App)
 app.use(router)
